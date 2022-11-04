ccd7ff0d86623503699bd22d146fefe9c7bd5375
DorianBadel <101052235+DorianBadel@users.noreply.github.com>
2022-11-04 22:39:58 +0000

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,23 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
+  darkMode: false,
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1080px',
+      '2xl': '1536px'
+    },
+    extend: {
+      colors: {
+        primary: "#ff4800",
+        secondary: {
+          regular: "#ff8800",
+          hover: "#ff9900"
+        }
+      }
+    },
   },
   plugins: [],
 }
